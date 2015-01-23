0bb14b2f201df0e593f42673cc7a6e556680a12a
Chris Richards <chris.richards@yellowfeather.co.uk>
2015-01-23 14:24:05 +0000

    Addresses compilation warnings, code tidy

--- src/hash.c
+++ src/hash.c
@@ -16,20 +16,9 @@
 #include "hash.h"
 #include "md5.h"
 
+static const int MD5_DIGEST_LENGTH = 16;
 
-const unsigned char *md5sum(const char *chaine, size_t len)
- {
-     struct md5_ctx ctx;
-     unsigned char digest[16];
-     md5_init(&ctx);
-     ctx.size = len?len:strlen(chaine);
-     strcpy(ctx.buf, chaine);
-     md5_update(&ctx);
-     md5_final(digest, &ctx);
-     return digest;
- }
-
- /* writeHashLine {{{
+/* writeHashLine {{{
  * creates a line in the hash document for each data set
  */
 int
@@ -37,11 +26,21 @@ writeHashLine(FILE *fp, P_DBF *p_dbf,
     const unsigned char *value, int record_length,
     const char *in /* unused */, const char *out /* unused */)
 {
-	const unsigned char *digest = md5sum(value, record_length);
+    struct md5_ctx ctx;
+    unsigned char digest[MD5_DIGEST_LENGTH];
 
-	for (int i = 0; i < 16; ++i) {
+    md5_init(&ctx);
+    ctx.size = record_length;
+    memcpy(ctx.buf, value, record_length);
+    md5_update(&ctx);
+    md5_final(digest, &ctx);
+
+    int i;
+	for (i = 0; i < 16; ++i) {
 		fprintf(fp, "%02x", digest[i]);
 	}
 
 	fputs("\n", fp);
+
+	return 0;
 }
